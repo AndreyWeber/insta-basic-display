2f8851ce24a2801d044491a333feaaa2a50ca6d7
Andrey Weber <andrey.weber@gmail.com>
2021-07-18 15:05:37 +0200

    Initial project structure

---
+++ desktop/InstaBasicDisplayConsole/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace InstaBasicDisplayConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey(false);
+        }
+    }
+}
