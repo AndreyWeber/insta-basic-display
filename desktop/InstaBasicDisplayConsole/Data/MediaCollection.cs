a7c6b73da79f52b42e70189110030376e7c0e1e0
Andrey Weber <Andrey.Weber@gmail.com>
2021-07-26 22:47:20 +0200

    Update MediaCollection.cs

--- desktop/InstaBasicDisplayConsole/Data/MediaCollection.cs
+++ desktop/InstaBasicDisplayConsole/Data/MediaCollection.cs
@@ -4,11 +4,7 @@ using Newtonsoft.Json;
 
 namespace InstaBasicDisplayConsole.Data
 {
-<<<<<<< HEAD
     public sealed class MediaCollection : InstagramEntityBase
-=======
-    public sealed class MediaCollection
->>>>>>> 560b841f76a6544d1cffbdbb9dee33834bad531d
     {
         [JsonProperty("data")]
         public IList<Media> Data { get; set; }
