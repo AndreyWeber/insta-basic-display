35eab9b1822e25a20a088da456c920f24cd5a22b
Andrey Weber <Andrey.Weber@gmail.com>
2021-07-26 22:46:29 +0200

    Update Cursors.cs

--- desktop/InstaBasicDisplayConsole/Data/Cursors.cs
+++ desktop/InstaBasicDisplayConsole/Data/Cursors.cs
@@ -4,11 +4,7 @@ using Newtonsoft.Json;
 
 namespace InstaBasicDisplayConsole.Data
 {
-<<<<<<< HEAD
     public sealed class Cursors : InstagramEntityBase
-=======
-    public sealed class Cursors
->>>>>>> 560b841f76a6544d1cffbdbb9dee33834bad531d
     {
         [JsonProperty("after")]
         public String After { get; set; }
