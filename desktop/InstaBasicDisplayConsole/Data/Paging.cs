0e600a3174d0d10023bcc5c81e9937c345874c06
Andrey Weber <Andrey.Weber@gmail.com>
2021-07-26 22:47:41 +0200

    Update Paging.cs

--- desktop/InstaBasicDisplayConsole/Data/Paging.cs
+++ desktop/InstaBasicDisplayConsole/Data/Paging.cs
@@ -4,11 +4,7 @@ using Newtonsoft.Json;
 
 namespace InstaBasicDisplayConsole.Data
 {
-<<<<<<< HEAD
     public sealed class Paging : InstagramEntityBase
-=======
-    public sealed class Paging
->>>>>>> 560b841f76a6544d1cffbdbb9dee33834bad531d
     {
         [JsonProperty("cursors")]
         public Cursors Cursors { get; set; }
